42d88cf7fa08157d28c5c910c28744e82c0ac60d
Lantz <lantzshaw@163.com>
2023-02-28 08:39:23 +0800

    feat: add new features

---
+++ shared/create.js
@@ -0,0 +1,17 @@
+// Block 块 article dialog sidebar form tab
+// Element form 里面的 input submit
+// Modifier disable theme-dark
+
+// .block 块 article dialog sidebar form tab
+// .block__element
+// .block--modifier 修饰
+// .block__element--modifier
+
+// lc-button lc-button-group
+// lc-button--primary lc-button--success lc-button--warning lc-button--danger
+// lc-button--disabled
+// lc-button--theme-dark
+
+function createBEM(element, modifier) {}
+
+createBEM('block', [])
